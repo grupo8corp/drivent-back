ed62536e27ee3436f24b79088269a87be8a14766
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-10-24 14:18:24 -0300

    refactor: seed das atividades alterada para ser mais realista e movida para a utils

--- src/utils/prisma-utils.ts
+++ src/utils/prisma-utils.ts
@@ -1,3 +1,7 @@
+import faker from "@faker-js/faker";
+import { Activity } from "@prisma/client";
+import dayjs from "dayjs";
+
 export function exclude<T, Key extends keyof T>(entity: T, ...keys: Key[]): Omit<T, Key> {
   const newEntity = JSON.parse(JSON.stringify(entity));
   for (const key of keys) {
@@ -5,3 +9,67 @@ export function exclude<T, Key extends keyof T>(entity: T, ...keys: Key[]): Omit
   }
   return newEntity;
 }
+
+type ActivityArray = Omit<Activity, 'id' | 'updatedAt' | 'createdAt'>
+export function seedActivityFactory(): ActivityArray[]{
+  return [
+    {
+      auditory: "LATERAL",
+      capacity: 5,
+      startsAt: dayjs().add(1, "hours").toDate(),
+      endsAt: dayjs().add(2, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    {
+      auditory: "MAIN",
+      capacity: 4,
+      startsAt: dayjs().add(2, "hours").toDate(),
+      endsAt: dayjs().add(3, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    {
+      auditory: "WORKSHOP",
+      capacity: 2,
+      startsAt: dayjs().add(5, "hours").toDate(),
+      endsAt: dayjs().add(7, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    {
+      auditory: "LATERAL",
+      capacity: 1,
+      startsAt: dayjs().add(3, "hours").toDate(),
+      endsAt: dayjs().add(6, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    
+
+    {
+      auditory: "LATERAL",
+      capacity: 1,
+      startsAt: dayjs().add(1, "days").add(3, "hours").toDate(),
+      endsAt: dayjs().add(1, "days").add(6, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    {
+      auditory: "LATERAL",
+      capacity: 5,
+      startsAt: dayjs().add(1, "days").add(1, "hours").toDate(),
+      endsAt: dayjs().add(1, "days").add(2, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    {
+      auditory: "MAIN",
+      capacity: 4,
+      startsAt: dayjs().add(1, "days").add(2, "hours").toDate(),
+      endsAt: dayjs().add(1, "days").add(3, "hours").toDate(),
+      name: faker.lorem.words(),
+    },
+    {
+      auditory: "WORKSHOP",
+      capacity: 2,
+      startsAt: dayjs().add(1, "days").add(5, "hours").toDate(),
+      endsAt: dayjs().add(1, "days").add(7, "hours").toDate(),
+      name: faker.lorem.words(),
+    }
+  ];
+};
